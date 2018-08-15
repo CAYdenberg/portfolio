d0ecdafe135556b978ffcaad8899209349e0b31b
@CAYdenberg <ydenberg@gmail.com>
2018-08-15 16:45:01 -0600

    Basic layout and design overhaul for homepage

--- routes/views/index.js
+++ routes/views/index.js
@@ -10,25 +10,8 @@ exports = module.exports = function(req, res) {
 	// item in the header navigation.
 	locals.section = 'home';
 	locals.data = {
-		posts: [],
 		projects: []
 	};
-
-	// Load the posts
-	view.on('init', function(next) {
-
-		var q = keystone.list('Post').model
-			.find()
-			.where('state', 'published')
-			.limit(3)
-			.sort('-publishedDate');
-
-		q.exec(function(err, results) {
-			locals.data.posts = results;
-			next(err);
-		});
-
-	});
 
 	// Load projects
 	view.on('init', function(next) {
@@ -38,7 +21,7 @@ exports = module.exports = function(req, res) {
 			.where('state', 'published');
 
 		q.exec(function(err, results) {
-			locals.data.projects = shuffle(results).slice(0, 2);
+			locals.data.projects = results;
 			next(err);
 		});
 

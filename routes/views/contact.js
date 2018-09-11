696b1f5bba50837fca634cec3db658ee23813ac6
@CAYdenberg <ydenberg@gmail.com>
2018-09-11 15:05:23 -0600

    Nodemailer contact form message

--- routes/views/contact.js
+++ routes/views/contact.js
@@ -1,6 +1,19 @@
 var keystone = require('keystone');
 var Enquiry = keystone.list('Enquiry');
 
+const nodemailer = require('nodemailer');
+
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: process.env.SMTP_USER, // generated ethereal user
+    pass: process.env.SMTP_PASSWORD // generated ethereal password
+  }
+});
+
 exports = module.exports = function(req, res) {
 
 	var view = new keystone.View(req, res);
@@ -9,7 +22,7 @@ exports = module.exports = function(req, res) {
 	// Set locals
 	locals.section = 'contact';
 	locals.formData = req.body || {};
-	locals.validationErrors = {};
+	locals.validationErrors = false;
 	locals.enquirySubmitted = false;
 	locals.pageTitle = "Contact me";
 
@@ -25,9 +38,16 @@ exports = module.exports = function(req, res) {
 			errorMessage: 'There was a problem submitting your enquiry:'
 		}, function(err) {
 			if (err) {
-				locals.validationErrors = err.errors;
+				locals.validationErrors = true;
 			} else {
 				locals.enquirySubmitted = true;
+				transporter.sendMail({
+					from: 'nodemailer@caseyy.org',
+					to: process.env.CONTACT_DESTINATION_ADDRESS,
+					subject: `Contact message from: ${req.body['name.full']}`,
+					text: req.body.message,
+					replyTo: req.body.email
+				})
 			}
 			next();
 		});

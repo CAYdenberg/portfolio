df0bece98c62a2c3a2ada8a5428486ebac1d05ca
@CAYdenberg <ydenberg@gmail.com>
2018-11-01 13:51:08 -0600

    Add today.js

---
+++ dist/today.js
@@ -0,0 +1,15 @@
+
+function setDateInput(date) {
+  document.getElementById('date-input').value = moment(date).format('YYYY-MM-DD');
+}
+
+ReactDOM.render(React.createElement(
+  'button',
+  {
+    type: 'button',
+    onClick: function onClick() {
+      return setDateInput(new Date());
+    }
+  },
+  'Today'
+), document.getElementById('date-picker'));
